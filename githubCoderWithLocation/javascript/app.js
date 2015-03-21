60683374fafa6645a23cfad648aa7c4fed5f8c7a
ybdesire <ybdesire@gmail.com>
2015-03-21 22:11:17 +0800

    Add new row for table by jQuery.

--- githubCoderWithLocation/javascript/app.js
+++ githubCoderWithLocation/javascript/app.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
-	
-	$.getJSON(
+$(document).ready(function () {
+
+    $.getJSON(
 		"https://api.github.com/search/users?q=+location:%22Nan%20Jing%22&page=1&per_page=1000",
-		function(data){
-			var i=0;
-			var str = "";
-			for(;i<data.items.length;i++)
-			{
-				str = str + data.items[i].html_url + "</br>"
-			}
-			$("p").html(str);
+		function (data) {
+		    var i = 0;
+		    var str = "";
+		    for (; i < data.items.length; i++) {
+		        str = str + data.items[i].html_url + "</br>"
+		    }
+		    $("p").html(str);
 		}
 	);
-
-	$("#infoTable tr:odd").addClass("todd");
-	$("#infoTable tr:even").addClass("teven");
+    //modify table row color
+    $("#infoTable tr:odd").addClass("todd");
+    $("#infoTable tr:even").addClass("teven");
+    //add new row at the end of table
+    var newRow = "<tr><td>30</td><td>31</td><td>32</td><td>33</td></tr>";
+    $("#infoTable tr:last").after(newRow);
 });
+
+
