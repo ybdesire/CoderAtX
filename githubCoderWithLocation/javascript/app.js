98874896ed51ef292027907c0f730d5438a1f4e9
ybdesire <ybdesire@gmail.com>
2015-03-22 22:38:04 +0800

    Get user email info.

--- githubCoderWithLocation/javascript/app.js
+++ githubCoderWithLocation/javascript/app.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
 
     $.getJSON(
-		"https://api.github.com/search/users?q=+location:%22Nan%20Jing%22&page=1&per_page=1000",
+		"https://api.github.com/search/users?q=+location:%22Nan%20jing%22&page=1&per_page=100",
 		function (data) {
 		    var i = 0;
 		    var str = "";
 		    for (; i < data.items.length; i++) {
-		        str = str + "<tr><td>" + data.items[i].html_url + "</td><td>31</td><td>32</td><td>33</td></tr>";
+		        var id_email = "email_id_" + i.toString();
+		        var githubURL = data.items[i].html_url;
+                str = str + "<tr><td>" + githubURL + "</td><td id='" + id_email + "'>31</td><td>32</td><td>33</td></tr>";
+
+		        //https://api.github.com/users/htoooth/events/public
+                var vURL = githubURL.split('/');
+                var user = vURL[vURL.length - 1];
+                console.log(user);
+                var urlForEmail = "https://api.github.com/users/" + user + '/events/public';
+                getEmailFromURL(urlForEmail, i);
 		    }
 		    //add new row at the end of table
 		    $("#infoTable tr:last").after(str);
@@ -18,10 +27,28 @@ $(document).ready(function () {
 	);
 
     //parse email from url
-    function getEmailFromURL(url)
+    function getEmailFromURL(url, i)
     {
-        $.get(url, function (data) {
+        $.getJSON(url, function (data) {
+            var j = 0;
+            var email = "";
 
+            for (; j < data.length; j++)
+            {
+                //if (typeof data[j].payload.commits.author.email != 'undefined')
+                //console.log(data[j].payload.commits.author.email);
+                if (data[j].hasOwnProperty("payload") && data[j].payload.hasOwnProperty("commits") && data[j].payload.commits[0].hasOwnProperty("author") && data[j].payload.commits[0].author.hasOwnProperty("email"))
+                {
+                    email = data[j].payload.commits[0].author.email;
+                    if(email!="")
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var id_email = "email_id_" + i.toString();
+            $("#"+id_email).html(email)
         });
     }
 
