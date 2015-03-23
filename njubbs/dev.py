68b002648b1a65b444d8c791e402fd874b72f5e7
ybdesire <ybdesire@gmail.com>
2015-03-23 23:08:28 +0800

    Parse the html page and get the a elements of topic page links.

--- njubbs/dev.py
+++ njubbs/dev.py
@@ -1,14 +1,29 @@
-import urllib.request
-import webbrowser
-import codecs
-from html.parser import HTMLParser
+import urllib.request           #get html content
+import webbrowser               #open browser to display html
+import codecs                   #encode
+from bs4 import BeautifulSoup   #parse html
+import re                       #regular expression
 
-resp = urllib.request.urlopen('http://bbs.nju.edu.cn/bbsdoc?board=JobAndWork&type=doc')
-content = resp.read()
-html = str(content, 'GBK').encode('gbk')
-#html = content
+def main():
+    board = "JobAndWork"
+    html = getURLHtmlContent('http://bbs.nju.edu.cn/bbsdoc?board={0}&type=doc'.format(board))
+    #displayHTMLAtBrowser(html)
+    soup = BeautifulSoup(html)
+    print(soup.findAll("a", attrs={"href":re.compile("bbscon")}))
+    
+    
+def getURLHtmlContent(url):
+    resp = urllib.request.urlopen(url)
+    content = resp.read()
+    html = str(content, 'GBK').encode('gbk')
+    return html
 
-f = codecs.open("hello.html", 'w', "gbk")
-f.write(html.decode("gbk"))
-f.close()
-webbrowser.open_new_tab("hello.html");
+def displayHTMLAtBrowser(html):
+    f = codecs.open("hello.html", 'w', "gbk")
+    f.write(html.decode("gbk"))
+    f.close()
+    webbrowser.open_new_tab("hello.html")
+
+
+if __name__=="__main__":
+    main()
