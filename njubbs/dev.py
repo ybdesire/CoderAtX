c260dd269bf1d9b39cd6d3816afbd86b49720750
ybdesire <ybdesire@gmail.com>
2015-03-24 05:22:00 -0400

    Parse the content of every ticket URL at home page.

--- njubbs/dev.py
+++ njubbs/dev.py
@@ -5,25 +5,40 @@ from bs4 import BeautifulSoup   #parse html
 import re                       #regular expression
 
 def main():
-    board = "JobAndWork"
+    board = 'JobAndWork'
+    contents = ['IT']
     html = getURLHtmlContent('http://bbs.nju.edu.cn/bbsdoc?board={0}&type=doc'.format(board))
     #displayHTMLAtBrowser(html)
     soup = BeautifulSoup(html)
-    print(soup.findAll("a", attrs={"href":re.compile("bbscon")}))
+    aTagArr = soup.findAll('a', attrs={'href':re.compile('bbscon')})
+    for element_a in aTagArr:
+        ticketURL = 'http://bbs.nju.edu.cn/' + element_a.get('href')
+        print(ticketURL)
+        isContentFromTicketURL(ticketURL, contents)                
+        
     
     
 def getURLHtmlContent(url):
     resp = urllib.request.urlopen(url)
     content = resp.read()
-    html = str(content, 'GBK').encode('gbk')
+    html = content
     return html
 
 def displayHTMLAtBrowser(html):
-    f = codecs.open("hello.html", 'w', "gbk")
-    f.write(html.decode("gbk"))
+    f = codecs.open('hello.html', 'w', 'gbk')
+    f.write(html.decode('gbk'))
     f.close()
-    webbrowser.open_new_tab("hello.html")
+    webbrowser.open_new_tab('hello.html')
+
+def isContentFromTicketURL(ticketURL, contents):
+    html = getURLHtmlContent(ticketURL)
+    soup = BeautifulSoup(html)
+    if soup.get_text().find(contents[0])!=-1:
+        print(soup.get_text())
+        return True
+    else:
+        return False
 
 
-if __name__=="__main__":
+if __name__=='__main__':
     main()
